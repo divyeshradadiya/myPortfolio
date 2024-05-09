ef90bd7b08298c7071b1548c70a776c6c9056a37
div- r- <120237020+divyeshradadiya@users.noreply.github.com>
2024-05-09 18:29:59 +0530

    Update data.ts

--- lib/data.ts
+++ lib/data.ts
@@ -51,12 +51,21 @@ export const experiencesData = [
     date: "2017 - 2018",
   },
   {
+    title: "Freelance",
+    location: "Remote",
+    description:
+      " I started my journey by freelancing and made projects according to client need. i learned a lot from this phase.",
+    icon: React.createElement(CgWorkAlt),
+    date: "2017 - 2018",
+  },
+  
+  {
     title: "Software Engineer Intern",
     location: "Bangalore, India",
     description:
       "Conducted code reviews and redesigned an API to enhance flexibility for third-party integration. • Implemented performance optimizations using React.js,Node.js and Express.js resulting in a reduction in load times and enhanced user experience. • collaborated on implementing new features and resolving bugs in existing codebases.",
     icon: React.createElement(FaReact),
-    date: "jan-2021 - july-2021",
+    date: "March-2021 - Oct-2021",
   },
   {
     title: "Associate Software Engineer",
@@ -64,7 +73,7 @@ export const experiencesData = [
     description:
       "Collaborating as part of a cross-functional team and contributed to the development of a web application using MERN stack, driving improvements in scalability and user engagement. • Utilized application to streamline state management and data querying processes, leading to 30% increment in application efficiency and smoother navigation. • Orchestrated seamless API integrations, leveraging Databases to ensure   Smooth data flow and fortify system resilience",
     icon: React.createElement(FaReact),
-    date: "july-2021 - july-2022",
+    date: "nov-2021 - oct-2022",
   },
   {
     title: "Software Engineer",
@@ -72,7 +81,7 @@ export const experiencesData = [
     description:
       "Built data-rich dashboard, seamlessly integrating Databases. Empowering users with actionable insights for informed decision-making. • Conceived and introduced the Automated Email Response System, which reduced response time to 1,000s email messages by 71%, resulting in increased email open rates and driving significant user interaction with the platform. • Implemented authentication for user account security, allowing seamless integration with email/password or Google provider, enhancing user data protection and platform trust. • Integrated wallet functionality into the payment feature, facilitating effortless fund management for users, thereby encouraging repeat transactions and strengthening user relationships. now days, looking for job whoch have remote culture.",
     icon: React.createElement(FaReact),
-    date: "July-2022 - Jan-2024",
+    date: "nov-2022 - april-2024",
   },
 ] as const;
 
