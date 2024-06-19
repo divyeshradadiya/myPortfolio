d6dfc28d5b5e320831d96508e4eaedb55215b399
div- r- <120237020+divyeshradadiya@users.noreply.github.com>
2024-06-19 11:52:21 +0530

    Update data.ts

--- lib/data.ts
+++ lib/data.ts
@@ -65,7 +65,7 @@ export const experiencesData = [
     description:
       "Conducted code reviews and redesigned an API to enhance flexibility for third-party integration. • Implemented performance optimizations using React.js,Node.js and Express.js resulting in a reduction in load times and enhanced user experience. • collaborated on implementing new features and resolving bugs in existing codebases.",
     icon: React.createElement(FaReact),
-    date: "March-2021 - Oct-2021",
+    date: "2021 - 2021",
   },
   {
     title: "Associate Software Engineer",
@@ -73,7 +73,7 @@ export const experiencesData = [
     description:
       "Collaborating as part of a cross-functional team and contributed to the development of a web application using MERN stack, driving improvements in scalability and user engagement. • Utilized application to streamline state management and data querying processes, leading to 30% increment in application efficiency and smoother navigation. • Orchestrated seamless API integrations, leveraging Databases to ensure   Smooth data flow and fortify system resilience",
     icon: React.createElement(FaReact),
-    date: "Nov-2021 - Oct-2022",
+    date: "2021 - 2022",
   },
   {
     title: "Software Engineer",
@@ -81,7 +81,7 @@ export const experiencesData = [
     description:
       "Built data-rich dashboard, seamlessly integrating Databases. Empowering users with actionable insights for informed decision-making. • Conceived and introduced the Automated Email Response System, which reduced response time to 1,000s email messages by 71%, resulting in increased email open rates and driving significant user interaction with the platform. • Implemented authentication for user account security, allowing seamless integration with email/password or Google provider, enhancing user data protection and platform trust. • Integrated wallet functionality into the payment feature, facilitating effortless fund management for users, thereby encouraging repeat transactions and strengthening user relationships. now days, looking for job whoch have remote culture.",
     icon: React.createElement(FaReact),
-    date: "Nov-2022 - April-2024",
+    date: "2022 - 2024",
   },
   {
     title: "Full-stack Engineer",
@@ -89,7 +89,7 @@ export const experiencesData = [
     description:
       "Collaborated with cross-functional teams to integrate with third-party APIs and ensure seamless integration with existing systems. Worked with a small engineering team to rapidly grow the platform, achieving 3x growth annually by building unique and innovative features. Discuss closely with product managers and UX designers to translate user feedback into impactful new features. Integrated new AI products to automate and simplify tasks like mailing, chatbots, data analysis, and personalized learning recommendations. Redesigning the frontend code to improve usability and accessibility, making it easier for students to navigate and understand the system. Working with Agile methodology, attending Daily Stand-ups, Discuss new product ideas and Features.",
     icon: React.createElement(FaReact),
-    date: "April-2024 - here",
+    date: "2024 - here",
   },
 ] as const;
 
